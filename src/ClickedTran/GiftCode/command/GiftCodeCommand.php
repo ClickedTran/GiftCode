67a6e8da02ab2235d3a8f63e694f531810fd89a8
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-08-14 23:32:52 +0700

    Upload `GiftCodeCommand`

---
+++ src/ClickedTran/GiftCode/command/GiftCodeCommand.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace ClickedTran\GiftCode\command;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use CortexPE\Commando\BaseCommand;
+use pocketmine\command\CommandSender;
+
+use ClickedTran\GiftCode\GiftCode;
+use ClickedTran\GiftCode\command\subcmd\HelpGiftCodeCommand;
+use ClickedTran\GiftCode\command\subcmd\CreateGiftCodeCommand;
+use ClickedTran\GiftCode\command\subcmd\RemoveGiftCodeCommand;
+use ClickedTran\GiftCode\command\subcmd\ListGiftCodeCommand;
+use ClickedTran\GiftCode\form\FormManager;
+
+class GiftCodeCommand extends BaseCommand{
+
+	private GiftCode $plugin;
+	
+	public function prepare(): void{
+	  $this->setPermission("giftcode.command");
+	  $this->registerSubcommand(new HelpGiftCodeCommand("help", "Help GiftCode Command"));
+	  $this->registerSubcommand(new CreateGiftCodeCommand("create", "Create GiftCode Command"));
+	  $this->registerSubcommand(new RemoveGiftCodeCommand("remove", "Remove GiftCode Command"));
+	  $this->registerSubcommand(new ListGiftCodeCommand("list", "List All GiftCode"));
+	}
+	
+	public function onRun(CommandSender $sender, String $labelUsed, Array $args): void{
+	  if(count($args) == 0 && $sender instanceof Player){
+	    if(!Server::getInstance()->isOp($sender->getName()) && !$sender->hasPermission("giftcode.command.menu")){
+	       $form = new FormManager();
+	       $sender->sendForm($form->menuEnterCode($sender));
+	    }else{
+	      $form = new FormManager();
+	      $sender->sendForm($form->menuGiftCode($sender));
+	    }
+	  }
+	}
+}

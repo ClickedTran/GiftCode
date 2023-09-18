5cf8cc85ff67e99100e3efe6285cdf2f6850bc91
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-09-18 10:30:32 +0700

    Update RemoveGiftCodeCommand.php

--- src/ClickedTran/GiftCode/command/subcmd/RemoveGiftCodeCommand.php
+++ src/ClickedTran/GiftCode/command/subcmd/RemoveGiftCodeCommand.php
@@ -19,11 +19,13 @@ class RemoveGiftCodeCommand extends BaseSubCommand {
   }
   
   public function onRun(CommandSender $sender, String $labelUsed, Array $args) : void{
-    if(!GiftCode::getInstance()->getCode()->exists($args["giftcode"])){
-       $sender->sendMessage("§9[ §4ERROR §9] §cGiftcode does not exist, please check and try again");
-    }
     if(!isset($args["giftcode"])){
        $sender->sendMessage("§9[ §4ERROR §9] §cPlease input giftcode name needs to be deleted!");
+       return;
+    }
+    if(!GiftCode::getInstance()->getCode()->exists($args["giftcode"])){
+       $sender->sendMessage("§9[ §4ERROR §9] §cGiftcode does not exist, please check and try again");
+       return;
     }
     GiftCode::getInstance()->removeCode($args["giftcode"]);
     $sender->sendMessage("§9[ §eSuccessfully §9]§a You deleted the giftcode §7" . $args["giftcode"] . " §afrom data");
